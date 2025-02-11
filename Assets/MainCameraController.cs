bdf22c58fac2f4f171f0bfde0722f78ef967a4f3
Heni <heni@udel.edu>
2025-02-10 21:26:44 -0500

    Message: Changed the text to a funny computer science joke

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Why do Java developers wear glasses? Because they don't C#");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
